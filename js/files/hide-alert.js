9715156d70cb32aca7fc672a0f5affd9313381b2
Jonas Faber <72028327+Jonohas@users.noreply.github.com>
2022-03-02 16:35:29 +0100

    Removed old files

--- js/files/hide-alert.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $(document).on("click", "#close-public-alert", function() {
-        Cookies.set('alertPublicSeen', 'true', { expires: 30, path: '' });
-    });
-});
