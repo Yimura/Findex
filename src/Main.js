56835827e91a25e36618a01366c09a544099e6de
Jonas Faber <72028327+Jonohas@users.noreply.github.com>
2022-03-03 11:04:45 +0100

    made it so that modules work

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,19 @@
 import Modules from 'waffle-manager';
+import log from '@/src/util/Logger.js'
 import { resolve as resolvePath } from 'path';
+import { loadJson } from '@/src/util/Util.js';
 
 export class Main {
     constructor() {
         ['beforeExit', 'SIGTERM', 'SIGINT'].map(signal => process.on(signal, this.cleanup.bind(this)));
+
+        this.config = loadJson('/data/config.json'); // Path based on root of project
+        const auth = loadJson('/data/auth.json');
+        Object.assign(this.config, auth);
+    }
+
+    get log() {
+        return log;
     }
 
     async cleanup() {
@@ -13,6 +23,6 @@ export class Main {
     }
 
     async init() {
-        await Modules.load(null, resolvePath('./src/modules/'));
+        await Modules.load(this, resolvePath('./src/modules/'));
     }
 }
